a795d639038aca047665a7aeb29e5bc26a124988
LauraForde <G00315765@gmit.ie>
2017-12-02 00:06:36 +0000

    Flasking reading in upload and displaying name to ensure it is that file

--- runner.py
+++ runner.py
@@ -1,11 +1,17 @@
-import flask as flsk
-from flask import render_template, request, url_for
+import flask as fl
+from flask import Flask, render_template, request
 
-app = flsk.Flask(__name__)
+app = fl.Flask(__name__)
 
 @app.route("/")
 def route():
-    return app.send_static_file('index.html')    
+    return app.send_static_file('index.html')
+
+# Adapted from https://www.youtube.com/watch?v=TLgVEBuQURA
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['img']
+    return file.filename
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
